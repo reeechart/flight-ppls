386dc7694e2c0ad0fb38220e71e1c00770e46c3d
reeechart <ferdinandusrichard@yahoo.co.id>
2018-11-10 20:32:47 +0700

    change flight model attributes

--- peoplesair/flight_booking/models.py
+++ peoplesair/flight_booking/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Flight(models.Model):
-    departure = models.CharField(max_length=255, blank=False)
-    arrival = models.CharField(max_length=255, blank=False)
-    departure_time = models.DateTimeField(auto_now=False)
+    origin = models.CharField(max_length=255, blank=False)
+    destination = models.CharField(max_length=255, blank=False)
+    departure = models.DateTimeField(auto_now=False)
+    arrival = models.DateTimeField(auto_now=False)
     available_seats = models.IntegerField(editable=True)
