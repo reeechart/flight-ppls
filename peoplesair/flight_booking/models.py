5291ec8ed91667cce576ae8f9f14e6e0f5c65a7b
reeechart <ferdinandusrichard@yahoo.co.id>
2018-11-10 20:16:39 +0700

    add flight model

--- peoplesair/flight_booking/models.py
+++ peoplesair/flight_booking/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Flight(models.Model):
+    departure = models.CharField(max_length=255, blank=False)
+    arrival = models.CharField(max_length=255, blank=False)
+    departure_time = models.DateTimeField(auto_now=False)
+    available_seats = models.IntegerField(editable=True)
