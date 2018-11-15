aa4dd127bd5f21864f42683e907a202022ac178e
Kenneth Halim <kanisiuskenneth@gmail.com>
2018-11-15 15:10:10 +0700

    Add admin interface for all entity

--- Entity/flight_booking/admin.py
+++ Entity/flight_booking/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from django.contrib.auth.models import User
+from .models import Flight, Invoice, Ticket, Booking
 # Register your models here.
+
+@admin.register(Flight)
+class FlightAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Invoice)
+class InvoiceAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    pass
