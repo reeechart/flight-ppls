44a3d044e7b6a9be78066232f6a8cab44d44bbf9
Kenneth Halim <kanisiuskenneth@gmail.com>
2018-11-16 12:18:11 +0700

    Add boilerplate for services tasks

--- Task/index.js
+++ Task/index.js
@@ -8,19 +8,22 @@ const config = { baseUrl: 'http://localhost:8080/engine-rest', use: logger };
 // create a Client instance with custom configuration
 const client = new Client(config);
 
-// susbscribe to the topic: 'charge-card'
-client.subscribe('reserve-seat', async function({ task, taskService }) {
-  // Put your business logic here
-  const processVariables = new Variables();
-  processVariables.set("reserved", false);
-
-  // Complete the task
-  await taskService.complete(task, processVariables);
+client.subscribe('book-create-booking', async function({ task, taskService }) {
+    const processVariables = new Variables();
+    processVariables.set('created', true);
+    await taskService.complete(task, processVariables);
 });
 
-client.subscribe('create-booking', async function({ task, taskService }) {
-  // Put your business logic here
-
-  // Complete the task
+client.subscribe('book-create-invoice', async function({ task, taskService }) {
   await taskService.complete(task);
 });
+
+client.subscribe('book-notify-payment-gateway', async function({ task, taskService }) {
+  await taskService.complete(task);
+});
+
+client.subscribe('book-cancel-booking', async function({ task, taskService }) {
+  await taskService.complete(task);
+});
+  
+  
